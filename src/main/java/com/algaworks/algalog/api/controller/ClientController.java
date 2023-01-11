d3c4ba559a3b4e0e6c1cf509047f20b17a547f44
EduardoRDS-dev <Eduardo_Rodrigues_Santos@protonmail.ch>
2023-01-10 23:34:43 -0300

    adding new endpoints for client.

--- src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -1,14 +1,15 @@
 package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.dto.ClientDTO;
+import com.algaworks.algalog.domain.model.Client;
 import com.algaworks.algalog.domain.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
@@ -19,5 +20,29 @@ public class ClientController {
     @GetMapping
     public ResponseEntity<List<ClientDTO>> findAll() {
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        return service.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<ClientDTO> add(@RequestBody Client client) {
+        return ResponseEntity.ok(service.save(client));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody Client client) {
+        return service.update(id, client).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        if (!service.delete(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/algaworks/algalog/domain/service/ClientService.java
+++ src/main/java/com/algaworks/algalog/domain/service/ClientService.java
@@ -1,11 +1,13 @@
 package com.algaworks.algalog.domain.service;
 
 import com.algaworks.algalog.domain.dto.ClientDTO;
+import com.algaworks.algalog.domain.model.Client;
 import com.algaworks.algalog.domain.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -15,5 +17,30 @@ public class ClientService {
 
     public List<ClientDTO> findAll() {
         return repository.findAll().stream().map(ClientDTO::new).toList();
+    }
+
+    public Optional<ClientDTO> findById(Long id) {
+        return repository.findById(id).map(ClientDTO::new).or(Optional::empty);
+    }
+
+    public ClientDTO save(Client client) {
+        return new ClientDTO(client);
+    }
+
+    public Optional<ClientDTO> update(Long id, Client client) {
+
+        if (findById(id).isPresent()) {
+            client.setId(id);
+            return Optional.of(save(client));
+        }
+        return Optional.empty();
+    }
+
+    public boolean delete(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
